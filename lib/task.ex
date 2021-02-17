8f177e878716539d7902f886a22fbe1e652c274b
Julien Urraca <julien.urraca@blockfi.com>
2021-02-17 15:18:31 -0600

    set up Task module and new Task changeset

--- lib/task.ex
+++ lib/task.ex
@@ -1,7 +1,21 @@
 defmodule Todo.Task do
-    defstruct [:title, :description, :due_date, :priority, :labels, :status, :is_recurring?]
+  use Ecto.Schema
+  import Ecto.Changeset
 
-    def new(fields \\ []) do
-      struct!(__MODULE__, fields)
-    end
+  schema "tasks" do
+    field :title, :string
+    field :description, :string
+    field :due_date, :utc_datetime
+    field :priority, :string, default: "low"
+    field :labels, {:array, :string}
+    field :status, :string, default: "open"
+    field :is_recurring?, :boolean, default: false
+  end
+
+  def new_changeset(task, params \\ %{}) do
+    task
+    |> cast(params, [:title, :description, :due_date, :priority, :labels, :status, :is_recurring?])
+    |> validate_required([:title, :description, :due_date])
+    |> unique_constraint(:title)
+  end
 end

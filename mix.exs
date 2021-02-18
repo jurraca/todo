03919910eee476222a318c3f4cc15aa7aad93764
Julien Urraca <julien.urraca@blockfi.com>
2021-02-18 12:20:38 -0600

    add ecto deps, a repo configuration, and an application file to supervise the repo

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Todo.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Todo.Application, []},
       extra_applications: [:logger]
     ]
   end
@@ -22,9 +23,8 @@ defmodule Todo.MixProject do
   defp deps do
     [
       {:ecto, "~> 3.5.7"},
-      {:ecto_sql, "~> 3.5.4"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.5.4"},
+      {:postgrex, ">=0.0.0"}
     ]
   end
 end

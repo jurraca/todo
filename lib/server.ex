1eb8c25221ab00be64bbb2b0aea13d9899317725
Julien Urraca <julien.urraca@blockfi.com>
2021-02-22 14:10:41 -0600

    initial genserver implementation

---
+++ lib/server.ex
@@ -0,0 +1,32 @@
+defmodule Todo.Server do
+    use GenServer
+    alias Todo.Task.Core
+
+    def start_link(opts) do
+      GenServer.start_link(__MODULE__, opts, name: Boundary)
+    end
+
+    def list_tasks() do
+      GenServer.call(Boundary, :list)
+    end
+
+    def quit() do
+      GenServer.stop(Boundary, :shutdown)
+    end
+
+    @impl true
+    def init(opts) do
+        {:ok, opts}
+    end
+
+    @impl true
+    def handle_call(:list, _from, _tasks) do
+        tasks = Core.list()
+        {:reply, tasks, tasks}
+    end
+
+    @impl true
+    def terminate(reason, state) do
+      {reason, state}
+    end
+end
